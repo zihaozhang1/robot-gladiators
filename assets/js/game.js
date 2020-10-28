38754c73b6b9c1c9c5d742ebaccf431508ea07e2
Zihao <zihaozhang1@gmail.com>
2020-10-28 02:04:14 -0400

    added name variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a window function named "prompt"
+var playerName = window.prompt("What is your Robot's name?")
+// What is this?
+console.log(playerName);
+// What console.log does
+console.log("This logs a string, good for leaving you a message");
+// This will do math and log 20
+console.log(10 + 10);
+// What is this?
+console.log("Our Robot's name is " + playerName);
+
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+
+}
+//fight();
